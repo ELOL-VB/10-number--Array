cdd7a97a7455ca2e7a272916b853a5a6da444a00
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-10 21:36:15 +0800

    Add files via upload

---
+++ 10個亂數產生(陣列)/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Randomize()
+        Dim a(9) As Integer
+        Dim x, y, j As Integer
+        Dim max, min As Integer
+        Dim str As String
+        str = ""
+        min = 100
+        x = 0
+        For x = 0 To 9
+            a(x) = Int(Rnd() * 100) + 0
+            str = a(x) & "," & str
+            y = a(x) + y
+            If a(x) > max Then max = a(x)
+            If a(x) < min Then min = a(x)
+        Next
+        Console.WriteLine("[" & str & "]")
+        Console.WriteLine("最大值" & max)
+        Console.WriteLine("最小值" & min)
+        Console.WriteLine("總和" & y)
+        Console.Read()
+    End Sub
+
+End Module
